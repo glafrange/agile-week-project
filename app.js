a0722f5e6cf43b3e9634482ce42cf7491d3f9da4
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-06-05 23:23:24 -0400

    add stock function, input and submit button

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+    $('#stock-input').keypress((event) => {
+        if(event.which == 13){
+            event.preventDefault();
+            $('#add-stock-btn').click();
+        }
+    });
+
+    $('#add-stock-btn').click(
+        () => {
+            const stockInput = $('#stock-input').val();
+            $('ul').append('<li>' + stockInput + '</li>');
+        }
+    );
+});
