384022ddc5a0e99d55784ea09af38be149cb6615
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-06-06 13:41:51 -0400

    Added display stock function and function to check Uppercase on Ticker

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
             $('#add-stock-btn').click();
         }
     });
-
+ 
     $('#add-stock-btn').click(
         () => {
             const stockInput = $('#stock-input').val();
@@ -25,10 +25,34 @@ $(document).ready(() => {
 
     // List of added stocks, with the names of users who own them
     const stocks = [
-        {ticker: 'AMZN', price: 1696, ownedBy: ['gabriel', 'joe']} // temp example data
+        {ticker: 'AMZN', price: 1696, ownedBy: ['gabriel', 'joe', 'chris', 'liz', 'gulia']},
+        {ticker: 'appl', price: 1696, ownedBy: ['gabriel', 'joe', 'connor']},
+        {ticker: 'micRO', price: 1696, ownedBy: ['gabriel', 'joe']},
+        {ticker: 'micRO', price: 1696, ownedBy: ['gabriel']}  // temp example data
     ];
+
+
+    // Sets ticker value toUpperCase if the are not already
+    const upperCaseTicker = array => {
+        array.forEach(stock => {
+            if (stock.ticker !== stock.ticker.toUpperCase()) {
+                stock.ticker = stock.ticker.toUpperCase();
+            }   
+        });
+        return array;
+    }
+
+    console.log(upperCaseTicker(stocks));
+
+
+    // Displays Stock to the content div
+    const displayStocks = array => {
+        array.forEach(stock => {
+            $("#content").append(`<div style="border-bottom: 1px black solid"><p>Ticker: ${stock.ticker}</p> <p>Price: ${stock.price}</p> <p>Owned by: ${stock.ownedBy}</p></div>`);
+        })
+    }
+
+    displayStocks(stocks);
+
 });
 
-const displayStock = array => {
-    return array;
-};
