1dd0305ae98b2e5538284204501621fce34b236f
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-06-05 23:29:49 -0400

    updated app.js with add stock function

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+$(document).ready(() => {
+    $('#stock-input').keypress((event) => {
+        if(event.which == 13){
+            event.preventDefault();
+            $('#add-stock-btn').click();
+        }
+    });
+
+    $('#add-stock-btn').click(
+        () => {
+            const stockInput = $('#stock-input').val();
+            $('ul').append('<li>' + stockInput + '</li>');
+        }
+    );
+});
+
 const displayStock = array => {
     return array;
 };
