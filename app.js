212c99da2d8a240409bd1f829de66f47d481c0c7
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-06-06 05:11:27 -0400

    added stocks array

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ $(document).ready(() => {
             $('ul').append('<li>' + stockInput + '</li>');
         }
     );
+
+    // List of added stocks, with the names of users who own them
+    const stocks = [
+        {ticker: 'AMZN', price: 1696, ownedBy: ['gabriel', 'joe']}
+    ];
 });
 
 const displayStock = array => {
