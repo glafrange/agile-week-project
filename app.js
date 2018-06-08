347cd7254b7700a96681132b42c7c278bcca2629
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-06-08 13:59:25 -0400

    uppercaseTicker function removed, relevant functions updated for new stock data structure

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 $(document).ready(() => {
 
     const stocks = {
-        XXXf: {price: 1750, name: 'Chris'},
-        vvvf: {price: 1139, name: 'Chris'},
-        rrrf: {price: 540, name: 'Chris'},
-        qqqf: {price: 666, name: 'Chris'},
-        ffff: {price: 666, name: 'Chris'}
+        XXXf: {ticker: 'XXXf', price: 1750, name: 'Chris'},
+        vvvf: {ticker: 'vvvf', price: 1139, name: 'Chris'},
+        rrrf: {ticker: 'rrrf', price: 540, name: 'Chris'},
+        qqqf: {ticker: 'qqqf', price: 666, name: 'Chris'},
+        ffff: {ticker: 'ffff', price: 666, name: 'Chris'}
     };
 
     class Stock {
-        constructor(ticker, price) {
+        constructor(ticker, price, name) {
             this.ticker = ticker;
             this.price = price;
             this.name = name;
@@ -33,7 +33,7 @@ $(document).ready(() => {
     // Gets form input value, current user, and stock price from API, 
     // saves as an object and pushes to the stock array
     $('#add-stock-btn').click(() => {
-            const stockInput = $('#stock-input').val();
+            const stockInput = $('#stock-input').val().toUpperCase();
             function callAPI() {
                 var query = $('#stock-input').val();
                 var url = "https://api.iextrading.com/1.0/stock/" + query + "/batch?types=price";
@@ -43,8 +43,7 @@ $(document).ready(() => {
                     dataType: "jsonp",
                     success: function(res) {
                         let newStock = new Stock(stockInput, res.price, currentUser);
-                        stocks.push(newStock);
-                        upperCaseTicker(stocks);
+                        stocks[stockInput] = newStock;
                         displayStocks(stocks);
                         console.log(stocks);
                 },
@@ -69,23 +68,12 @@ $(document).ready(() => {
     });
 }); */
 
-
-    // Sets ticker value toUpperCase if the are not already
-    const upperCaseTicker = array => {
-        array.forEach(stock => {
-            if (stock.ticker !== stock.ticker.toUpperCase()) {
-                stock.ticker = stock.ticker.toUpperCase();
-            }   
-        });
-        return array;
-    };
-
     // Displays Stock 
     const displayStocks = obj => {
         $("tbody").empty();
         for(let key in stocks){
             if (!stocks.hasOwnProperty(key)) continue;
-            let keyData = `<tr id=${key}> <td>${key}</td>`;
+            let keyData = `<tr id=${key}>`;
         
             let obj = stocks[key];
             for(let prop in obj) {
