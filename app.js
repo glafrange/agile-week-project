b534da760d130bae926c8bf9c1a8b52001af6081
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-06-11 13:54:48 -0400

    fixed delete function, added td css

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ $(document).ready(() => {
     };
     
     const delStocks = () =>{     
-    $('#delStocks').click(function(event){
+    $('.delStocks').click(function(event){
     var stockName = $(event.target).closest('tr').find("[prop='ticker']").html();
     delete stocks[stockName];
     console.log(stockName);
@@ -98,7 +98,7 @@ $(document).ready(() => {
                 if (!obj.hasOwnProperty(prop)) continue;
                  keyData += `<td prop=${prop}>${obj[prop]}</td> `;
             }
-            keyData += `<td align="center"><button id='delStocks' type="button">Thing </button>`;
+            keyData += `<td align="center"><button class='delStocks' type="button">Thing </button></td>`;
             keyData += "</tr>";
             $("tbody").append(keyData );
             $("tbody tr:last-child").hide();
