3313df329ccb1295b480b22227cd74106e0210e5
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-06-18 13:55:18 -0400

    autofocus to modal input

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ $(document).ready(() => {
         }
     });
 
+    // Every time a modal is shown, if it has an autofocus element, focus on it.
+    $('.modal').on('shown.bs.modal', function() {
+        $(this).find('[autofocus]').focus();
+      });
+
     
     // ToolTip Function 
     $(function () {
